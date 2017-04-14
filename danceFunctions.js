b484b1066f10e4390aeb2704a4b60291eed808fc
RouterBox <routerbox@gmail.com>
2017-04-14 15:13:48 -0500

    patterns and pallete

--- danceFunctions.js
+++ danceFunctions.js
@@ -83,3 +83,184 @@ function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function paintPattern(patternArray){
+	for(var i = 1; i < 11; i++){
+		var columnArray = patternArray[i-1];
+		chooseWhichOne("huzzah"+i"/");
+		for(var j = 0; j < 10; j++){
+			var boxColor = columnArray[j];
+			var box = j+1;
+			var red = boxColor.r;
+			var green = boxColor.g;
+			var blue = boxColor.b;
+			var message = '{\"red\":'+red+', \"green\":'+green+', \"blue\":'+blue+', \"box\":'+box+'}'
+			publish("ten", message);
+		}
+	}
+}
+
+
+///////////////////////////////////////////////////PATTERNS!!!!////////////////////////////////
+
+function heart1(){
+	var r1 = {"r":255 "g":0 "b":0 };
+	var r2 = {"r":192 "g":0 "b":0 };
+	var w = {"r":64 "g":64 "b":64 };
+	var b = {"r":0 "g":0 "b":0 };
+	
+	var arr = [
+		[w,w,w,w,b,b,b,b,w,w],
+		[w,w,b,r1,r1,r1,r1,b,w],
+		[w,w,b,r1,r2,r2,r2,r2,r1,b],
+		[w,b,r1,r2,r2,r2,r2,r2,r1,b],
+		[b,r1,r2,r2,r2,r2,r2,r1,b,w],
+		[b,r2,r2,r2,r2,r2,r2,b,b,w],
+		[w,b,r2,r2,r2,r2,r2,r1,r1,b],
+		[w,w,b,r2,r2,r2,r2,r2,r1,b],
+		[w,w,w,b,r2,r2,r2,r2,b,w],
+		[w,w,w,w,b,b,b,b,w,w]
+	]
+	return arr;
+}
+
+function spaceInvader1(){
+	var q = {"r":112 "g":48 "b":160 };
+	var w = {"r":0 "g":0 "b":0 };
+	
+	var arr = [
+		[w,w,w,w,w,w,w,w,w,w],
+		[w,w,q,w,q,q,w,w,w,w],
+		[w,q,w,q,q,q,q,w,w,w],
+		[w,w,w,w,q,w,q,q,w,w],
+		[w,w,w,q,q,q,q,q,q,w],
+		[w,w,w,q,q,q,q,q,q,w],
+		[w,w,w,w,q,w,q,q,w,w],
+		[w,q,w,q,q,q,q,w,w,w],
+		[w,w,q,w,q,q,w,w,w,w],
+		[w,w,w,w,w,w,w,w,w,w]
+	]
+	return arr;
+}
+function spaceInvader2(){
+	var q = {"r":0 "g":176 "b":240 };
+	var w = {"r":0 "g":0 "b":0 };
+	
+	var arr = [
+		[w,w,w,w,w,w,w,w,w,w],
+		[w,q,w,w,q,q,w,w,w,w],
+		[w,w,q,w,q,q,q,w,w,w],
+		[w,q,w,q,q,w,q,q,w,w],
+		[w,w,q,w,q,q,q,q,q,w],
+		[w,w,q,w,q,q,q,q,q,w],
+		[w,q,w,q,q,w,q,q,w,w],
+		[w,w,q,w,q,q,q,w,w,w],
+		[w,q,w,w,q,q,w,w,w,w],
+		[w,w,w,w,w,w,w,w,w,w]
+	]
+	return arr;
+}
+function spaceInvader3(){
+	var q = {"r":112 "g":48 "b":160 };
+	var w = {"r":0 "g":0 "b":0 };
+	
+	var arr = [
+		[w,w,w,w,q,q,q,q,w,w],
+		[w,q,w,q,q,q,q,w,w,w],
+		[w,w,q,q,q,q,q,q,w,q],
+		[w,w,w,q,q,q,w,q,q,w],
+		[w,w,w,q,q,q,q,q,w,w],
+		[w,w,w,q,q,q,q,q,w,w],
+		[w,w,w,q,q,q,w,q,q,w],
+		[w,w,q,q,q,q,q,q,w,q],
+		[w,q,w,q,q,q,q,w,w,w],
+		[w,w,w,w,q,q,q,q,w,w]
+	]
+	return arr;
+}
+
+function smileyFace(){
+	var b = {"r":0 "g":0 "b":0 };
+	var y = {"r":128 "g":128 "b":0 };
+	var w = {"r":128 "g":128 "b":128 };
+	var r = {"r":64 "g":0 "b":0 };
+	
+	var arr = [
+		[b,b,y,y,y,y,y,y,b,b],
+		[b,y,y,y,w,y,y,y,y,b],
+		[y,y,y,w,y,y,w,w,y,y],
+		[y,y,w,y,y,y,w,w,y,y],
+		[y,y,w,y,y,y,y,y,y,y],
+		[y,y,w,y,y,y,y,y,y,y],
+		[y,y,w,y,y,y,w,w,y,y],
+		[y,y,y,w,y,y,w,w,y,y],
+		[b,y,y,y,w,y,y,y,y,b],
+		[b,b,y,y,y,y,y,y,b,b]
+	]
+	return arr;
+}
+function smileyFrench(){
+	var b = {"r":0 "g":0 "b":0 };
+	var y = {"r":128 "g":128 "b":0 };
+	var w = {"r":128 "g":128 "b":128 };
+	var r = {"r":64 "g":0 "b":0 };
+	
+	var arr = [
+		[b,b,y,y,y,y,y,y,b,b],
+		[b,y,y,y,w,y,y,y,y,b],
+		[y,y,y,w,y,y,w,w,y,y],
+		[y,y,w,y,y,y,w,w,y,y],
+		[y,r,r,y,y,y,y,y,y,y],
+		
+		[y,r,r,y,y,y,y,y,y,y],
+		[y,y,w,y,y,y,w,w,y,y],
+		[y,y,y,w,y,y,w,w,y,y],
+		[b,y,y,y,w,y,y,y,y,b],
+		[b,b,y,y,y,y,y,y,b,b]
+	]
+	return arr;
+}
+
+function crabGuy1(){
+	var b = {"r":0 "g":176 "b":240 };
+	var r = {"r":64 "g":0 "b":0 };
+	var w = {"r":128 "g":128 "b":128 };
+	var k = {"r":0 "g":0 "b":0 };
+	
+	var arr = [
+		[b,b,b,b,r,r,r,r,r,r],
+		[r,b,b,b,r,b,b,b,r,b],
+		[b,r,r,r,r,w,w,b,b,r],
+		[r,r,r,r,r,k,w,b,b,b],
+		[b,r,r,r,r,b,b,b,b,b],
+		
+		[b,r,r,r,r,b,b,b,b,b],
+		[r,r,r,r,r,w,w,b,b,b],
+		[b,r,r,r,r,k,w,b,b,r],
+		[r,b,b,b,r,b,b,b,r,b],
+		[b,b,b,b,r,r,r,r,r,r]
+	]
+	return arr;
+}
+function crabGuy2(){
+	var b = {"r":0 "g":176 "b":240 };
+	var r = {"r":64 "g":0 "b":0 };
+	var w = {"r":128 "g":128 "b":128 };
+	var k = {"r":0 "g":0 "b":0 };
+	
+	var arr = [
+		[b,b,b,b,r,r,r,r,r,b],
+		[r,b,b,b,r,b,b,r,b,b],
+		[b,r,r,r,r,k,w,b,r,b],
+		[r,r,r,r,r,w,w,b,b,b],
+		[b,r,r,r,r,b,b,b,b,b],
+		[b,r,r,r,r,b,b,b,b,b],
+		[r,r,r,r,r,k,w,b,b,b],
+		[b,r,r,r,r,w,w,b,r,b],
+		[r,b,b,b,r,b,b,r,b,b],
+		[b,b,b,b,r,r,r,r,r,b]
+	]
+	return arr;
+}
+
+
